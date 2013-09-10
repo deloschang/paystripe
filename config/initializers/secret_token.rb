d5631a3bb2eec417411fc5f36843ac3a13bd856e
Delos Chang <delosfchang@gmail.com>
2013-09-09 20:34:24 -0700

    basic rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Okcpay::Application.config.secret_token = '0149cc597546e838350c921b9d140eeb7c9a1394b807a642c42e5fd10159f16e03ba59f7799ba9401f457eded9d23c256f05567ab5f6b8ebe320ef3825791691'
