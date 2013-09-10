86a44c21c3f93435694667ac6928bafa98c136cc
Delos Chang <delosfchang@gmail.com>
2013-09-09 23:03:37 -0700

    display the amount paid as a confirmation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,12 +5,16 @@ class ChargesController < ApplicationController
   # add endpoint for each sales to differentiate by POST request
   def josh
     render :action => :new
+
+    # Possible description identifier in future
+    #@sales = 'josh'
   end
 
   def create
     # Amount in cents
     # Very basic level of sanitization
     @amount = params[:amount].to_i
+    #@sales = params[:sales]
     
     if @amount == 500 || @amount == 2000 || @amount == 5000
       customer = Stripe::Customer.create(
@@ -21,14 +25,14 @@ class ChargesController < ApplicationController
       charge = Stripe::Charge.create(
         :customer   => customer.id,
         :amount     => @amount,
-        :description => 'Payment Customer',
+        #:description => @sales,
         :currency => 'usd'
       )
     end
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to charges_path
+      redirect_to charges_path, :amount => @amount
     end
     
 end
